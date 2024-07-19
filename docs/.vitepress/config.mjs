0176032333a76f6fd74c3bbbc93a6f743149ef10
tsra0ne <tsravangoud164@gmail.com>
2024-07-19 14:17:51 +0530

    changed url base

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "The Swift Guy",
   description: "Swift, SwiftUI, UIKit",
-  base: "/theswiftguy/",
+  base: "/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
